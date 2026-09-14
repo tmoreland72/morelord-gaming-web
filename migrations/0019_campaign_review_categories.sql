cfbdda575a634370b7b3e1745da0421c9804ca10
tmoreland72 <troy@morelands.net>
2026-09-14 11:53:58 -0500

    Remove obsolete Campaign Manager migrations and retirement notes

--- migrations/0019_campaign_review_categories.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE campaign_proposals ADD COLUMN category TEXT NOT NULL DEFAULT 'reference' CHECK(category IN ('location','npc','quest','faction','reference'));
-CREATE TRIGGER campaign_category_immutable BEFORE UPDATE OF category ON campaign_proposals
-WHEN NEW.category IS NOT OLD.category
-BEGIN
- SELECT RAISE(ABORT, 'campaign_proposal_immutable');
-END;
